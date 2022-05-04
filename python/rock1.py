6cf55bdb12d9fbccf8594df5463fcd17037e289b
VATSAL MEHTA <91190459+Vatsalm038@users.noreply.github.com>
2022-05-04 07:56:39 +0530

    Add files via upload

---
+++ python/rock1.py
@@ -0,0 +1,67 @@
+'''a=10;b=12
+if(a<b):
+    print("a is smaller than b")
+elif(a>b):
+    print("a is greater than b")
+elif(a==b):
+    print("a and b are equal")
+print("If block executed")
+'''
+
+'''r= range(5)
+print(r)
+for i in range(2,20,5):
+    print(i)
+
+course="python"
+for i in range(len(course)):
+    print(course[i])
+
+element=int(input("Enter the number"))
+if element in [1,2,3,4]:
+    print("number present")
+else:
+    print("number","not", "present" ,sep = "--")
+ 
+'''
+'''lst=[1,2,3,4]
+print(lst)
+for index,element in enumerate(lst,start=0):
+    print(index,element)
+
+lst.append(["java",1.5])
+print(lst)
+print(lst[3])
+'''
+'''t= 1,2,3,"abc"
+t2=4,5,6,7
+result=t+t2
+print(result)
+n=int(input("Enter the number of the elements"))
+tmp=[]
+for i in range(n):
+    ele=int(input("enter th element:"))
+    tmp.append(ele)
+print(tmp)
+'''
+#d={'Name': 'XYZ', 'Company': 'TSEC', 'Designation': 'AP', 'Marks': [70, 65, 45]}
+#for key in d.keys():
+ #   print(key, ":",d[key])
+
+
+'''
+for key,values in d.items():
+    print(key,":",value)
+'''
+
+'''
+j=input("enter the key")
+mydict[j]=input("enter the value")
+print(mydict)
+'''
+
+
+
+
+
+
